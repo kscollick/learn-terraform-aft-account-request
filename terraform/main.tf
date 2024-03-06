31e2cadeaf0399195ad0a223ff1b93ce91dfbe4a
kscollick <71855829+kscollick@users.noreply.github.com>
2024-03-06 14:28:36 -0700

    Update main.tf
    Trying again to create a new AWS account using Account Factory for Terraform.

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "publicweb" {
 
   account_customizations_name = "publicweb"
 }
+
+module "publicwebhost" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-account+pubweb_zeqw12gh@c-path.org"
+    AccountName               = "C-Path Prod Public Website"
+    ManagedOrganizationalUnit = "US"
+    SSOUserEmail              = "kscollick@c-path.org"
+    SSOUserFirstName          = "Keith"
+    SSOUserLastName           = "Scollick"
+  }
+
+  account_tags = {
+    "Environment" = "Prod"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "DRS-246"
+    change_reason       = "Create account for hosting public website media"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "publicwebhost"
+}
