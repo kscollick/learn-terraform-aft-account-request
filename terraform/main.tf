4eab3f11e0443530ecf7a2026c6cdaf1d25cf22c
Josh Bowen <jbowen@c-path.org>
2024-03-06 13:38:58 -0700

    pubweb try without sandbox entry

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,3 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "aws-account+sandbox_asdiasdldie@c-path.org"
-    AccountName               = "C-Path Public Website"
-    ManagedOrganizationalUnit = "US"
-    SSOUserEmail              = "jbowen@c-path.org"
-    SSOUserFirstName          = "Josh"
-    SSOUserLastName           = "Bowen"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
 module "pubweb" {
   source = "./modules/aft-account-request"
 
