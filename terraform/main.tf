0a12be11bb49c3799b30b9544feba4aba69e5dd3
Josh Bowen <jbowen@c-path.org>
2024-03-06 12:07:50 -0700

    Public website deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "aws-account+pubweb_fhygu@c-path.org"
+    AccountEmail              = "aws-account+sandbox_asdiasdldie@c-path.org"
     AccountName               = "C-Path Public Website"
     ManagedOrganizationalUnit = "US"
     SSOUserEmail              = "jbowen@c-path.org"
@@ -24,4 +24,31 @@ module "sandbox" {
   }
 
   account_customizations_name = "sandbox"
+}
+module "pubweb" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-account+pubweb_fhygu@c-path.org"
+    AccountName               = "C-Path Public Website"
+    ManagedOrganizationalUnit = "US"
+    SSOUserEmail              = "jbowen@c-path.org"
+    SSOUserFirstName          = "Josh"
+    SSOUserLastName           = "Bowen"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "pubweb"
 }
