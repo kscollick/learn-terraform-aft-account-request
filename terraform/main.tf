a6f73ef22da2374a94f7b9d2f9aba7668abfd36b
Josh Bowen <jbowen@c-path.org>
2025-07-30 12:20:17 -0700

    Comment out RDCA again

--- terraform/main.tf
+++ terraform/main.tf
@@ -540,38 +540,38 @@ module "sandboxayan" {
   account_customizations_name = "sandboxayan"
   }
 
-module "cpathrdcaimport" {
-  source = "aws-ia/aft-account-request/aws"
-
-  control_tower_parameters = {
-    AccountEmail              = "aws-account+rdca@c-path.org"
-    AccountName               = "C-Path RDCA"
-    ManagedOrganizationalUnit = "US - Control Tower"
-    SSOUserEmail              = "jbowen@c-path.org"
-    SSOUserFirstName          = "Josh"
-    SSOUserLastName           = "Bowen"
-  }
-
-  account_request_parameters = {
-    request_type = "import"
-    account_id   = "417631757239"
-  }
-
-  account_tags = {
-    "Environment" = "prod"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "platform #16587"
-    change_reason       = "Add RDCA to AFT"
-  }
-
-custom_fields = {
-    group = "prod"
-  }
-
-  account_customizations_name = "cpathrdcaimport"
-  }
+#module "cpathrdcaimport" {
+##  source = "aws-ia/aft-account-request/aws"
+#
+#  control_tower_parameters = {
+#    AccountEmail              = "aws-account+rdca@c-path.org"
+#    AccountName               = "C-Path RDCA"
+#    ManagedOrganizationalUnit = "US - Control Tower"
+#    SSOUserEmail              = "jbowen@c-path.org"
+#    SSOUserFirstName          = "Josh"
+#    SSOUserLastName           = "Bowen"
+#  }
+#
+#  account_request_parameters = {
+#    request_type = "import"
+#    account_id   = "417631757239"
+#  }
+#
+#  account_tags = {
+#    "Environment" = "prod"
+#  }
+#
+#  change_management_parameters = {
+#    change_requested_by = "platform #16587"
+#    change_reason       = "Add RDCA to AFT"
+#  }
+#
+#custom_fields = {
+#    group = "prod"
+#  }
+#
+#  account_customizations_name = "cpathrdcaimport"
+#  }
 
 module "fileuploadertestnew0725" {
   source = "./modules/aft-account-request"
