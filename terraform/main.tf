12edc066405843feb477ca62da47b77235bc4284
Josh Bowen <jbowen@c-path.org>
2026-01-21 11:20:39 -0700

    remove old gitlab test account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,3 @@
-module "gitlabtest" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "aws-account+gitlabtest_fyutes@c-path.org"
-    AccountName               = "c-path-gitlab-test"
-    ManagedOrganizationalUnit = "US - Control Tower"
-    SSOUserEmail              = "jbowen@c-path.org"
-    SSOUserFirstName          = "Josh"
-    SSOUserLastName           = "Bowen"
-  }
-
-  account_tags = {
-    "Environment" = "Test"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "DCC#34"
-    change_reason       = "Create account"
-  }
-
-  custom_fields = {
-    group = "prod"
-  }
-
-  account_customizations_name = "gitlabtest"
-}
 module "fileuploadertest" {
   source = "./modules/aft-account-request"
 
