c30fcf5a0422022431c211a9e2463c61d814a4e3
Josh Bowen <jbowen@c-path.org>
2026-01-22 11:03:55 -0700

    Create Posit account

--- terraform/main.tf
+++ terraform/main.tf
@@ -511,3 +511,30 @@ module "cpathcodrus" {
 
   account_customizations_name = "cpathcodrus"
 }
+module "cpathpoistpoc" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-account+poistpoc_yhtnurm@c-path.org"
+    AccountName               = "C-Path POSIT POC"
+    ManagedOrganizationalUnit = "US - Control Tower"
+    SSOUserEmail              = "jbowen@c-path.org"
+    SSOUserFirstName          = "Josh"
+    SSOUserLastName           = "Bowen"
+  }
+
+  account_tags = {
+    "Environment" = "Prod"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "#18826"
+    change_reason       = "create account"
+  }
+
+  custom_fields = {
+    group = "test"
+  }
+
+  account_customizations_name = "cpathpoistpoc"
+}
